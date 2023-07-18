5972b365ca3c4aedcabddd86c12c39826c2c09ee
Sabirul Islam <45106497+mdsabir07@users.noreply.github.com>
2023-07-18 16:37:12 +0600

    commit

--- eloquent-orm/app/Http/Controllers/DemoController.php
+++ eloquent-orm/app/Http/Controllers/DemoController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    //
+    public function DemoAction(Request $request){
+        return Product::sum('price');
+    }
 }
